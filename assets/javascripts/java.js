9f5444812c9d42b75edafa1efe30e0b8a2a49dcf
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-04-24 00:10:20 -0700

    http has updated with https and comments were made

--- assets/javascripts/java.js
+++ assets/javascripts/java.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+    //Create an array that contains the default items.
     var arr = ['hamburger', 'hotdog', 'french fries', 'taco', 'burrito', 'milk shake', 'sushi', 'ramen', 'bulgogi', 'korean bbq', 'jamon', 'paella', 'pad thai']
 
+    //This start fuction will iterate the array of items and display them as button
     function start() {
         for (var i = 0; i < arr.length; i++) {
             var but = $("<button type='button' class='btn btn-info'>");
@@ -8,18 +10,23 @@ $(document).ready(function() {
             $('#buttonBox').append(but);
         }
     }
+    // this will invoke the start function
     start();
 
+    //This will target the class btn and when its clicked it will invoke the gifs function
+    $(document).on('click', '.btn', gifs);
+    //Whenever the img elements are clicked, clicks function will invoke.
+    $(document).on('click', 'img', stilloranimate);
+    // This on click method will display user input as button
     $("#add").on('click', function() {
         var name = $('#inputBox').val().trim();
         var but = $("<button type='button' class='btn btn-info'>");
         but.html(name);
         $('#buttonBox').append(but)
     });
-    $(document).on('click', '.btn', gifs);
-    $(document).on('click', 'img', clicks);
-
-    function clicks() {
+    
+    //this function checks whether the gifs are still or animate and change the status of gifs
+    function stilloranimate() {
 
         if ($(this).data('status') == 'still') {
             $(this).attr('src', $(this).data('animate'));
@@ -30,10 +37,11 @@ $(document).ready(function() {
         }
     }
 
+    //This function will use the button's text value and input them to the ajax to grab JSON and pick the infos we want and display the gifs.
     function gifs() {
         var animal = $(this).html();
         console.log(animal)
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             animal + "&api_key=dc6zaTOxFJmzC&limit=10";
         console.log(queryURL)
         $.ajax({
